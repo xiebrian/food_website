7aeeaa94cd2d30054e4d6179ecd62383ac8ff627
xiebrian <brianxie98@gmail.com>
2024-04-30 13:56:11 -0700

    fix

--- cookbook/views.py
+++ cookbook/views.py
@@ -57,12 +57,14 @@ def index(request):
     # Pagination
     paginator = Paginator(recipes_to_display, 25)
     page_number = request.GET.get("page")
+    if page_number is None:
+        page_number = 1
     recipes_to_display_paginated = paginator.get_page(page_number)
     page_range = paginator.get_elided_page_range(page_number, on_each_side=3, on_ends=1)
 
     context = {
         'recipes_to_display': recipes_to_display_paginated,
-        'current_page_number': int(1) if page_number is None else int(page_number),
+        'current_page_number': page_number,
         'page_range': page_range,
         'cuisines': cuisines,
         'categories': categories,
