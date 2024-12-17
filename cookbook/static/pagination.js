66c78420825e1dde3c7ede8fecb1d8247fa0ab61
xiebrian <brianxie98@gmail.com>
2024-12-17 08:06:32 -0800

    lets give this a try

---
+++ cookbook/static/pagination.js
@@ -0,0 +1,18 @@
+function addOrUpdateUrlParam(name, value)
+{
+  var href = window.location.href;
+  console.log(href);
+  var regex = new RegExp("[&\\?]" + name + "=");
+  if(regex.test(href))
+  {
+    regex = new RegExp("([&\\?])" + name + "=\\d+");
+    return href.replace(regex, "$1" + name + "=" + value);
+  }
+  else
+  {
+    if(href.indexOf("?") > -1)
+      return href + "&" + name + "=" + value;
+    else
+      return href + "?" + name + "=" + value;
+  }
+}
